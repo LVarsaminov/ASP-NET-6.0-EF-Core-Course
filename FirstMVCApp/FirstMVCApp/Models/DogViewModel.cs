44533dbae4216433d3a84a2e54e35ee98fca0c11
Leonid Varsaminov <108093053+LVarsaminov@users.noreply.github.com>
2022-08-30 14:27:29 +0300

    Add files via upload

---
+++ FirstMVCApp/FirstMVCApp/Models/DogViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApp.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
