17635784f79f75995e0887dc6465207ed0f0b712
Leonid Varsaminov <108093053+LVarsaminov@users.noreply.github.com>
2022-08-30 14:23:21 +0300

    Add files via upload

---
+++ RazorPizzeria/RazorPizzeria/Models/PizzaOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPizzeria.Models
+{
+    public class PizzaOrder
+    {
+        public int Id { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; }
+    }
+}
